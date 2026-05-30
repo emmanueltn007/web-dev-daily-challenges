27a07aaa09f5f964886bace1d090dea51daf1cd8
emmanueltn007 <emmanuelnotyhawe@gmail.com>
2026-05-30 19:51:59 +0200

    Made text responsive in the hero section

--- recipe-application/src/components/Hero.jsx
+++ recipe-application/src/components/Hero.jsx
@@ -2,10 +2,10 @@ function Hero () {
     return (
         <section className="text-center bg-gray-300 px-8 py-8 lg:px-32 flex flex-col gap-4">
             <img loading="lazy" src="https://cdn.dummyjson.com/recipe-images/5.webp" alt="" />
-            <h1 className="text-3xl font-light">
+            <h1 className="text-4xl md:text-3xl lg:2xl font-light">
                 Master The Kitchen With Ease: Unleash Your Inner Chef Today!
             </h1>
-            <p>
+            <p className="text-lg lg:sm">
                 Discover recipes helping you find the easiest way to cook.
             </p>
             <button className="bg-orange-400 px-4 py-2 text-white rounded-md cursor-pointer self-center">
